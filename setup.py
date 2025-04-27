5cf309bdccb470bc35f3f996668228f2d7503155
Dare  Badejo <darebadejo@Mac.lan>
2025-04-27 14:15:16 -0400

    updated with src, setup and requiremens

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages 
+from typing import List
+def get_requirements(file_path: str) -> List[str]:
+    """
+    This function returns a list of requirements
+    """
+    requirements = []
+    with open(file_path) as file:
+        requirements = file.readlines()
+        requirements = [req.replace('\n', '') for req in requirements]
+        if '-e .' in requirements:
+            requirements.remove('-e .')
+    return requirements
+
+setup(
+    name='my_package',
+    version='0.0.1',
+    author='DareBadejo',
+    author_email='ooa.badejo@gmail.com', 
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt'),
+)
