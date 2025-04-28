bd37e08a13615e24a92ef75642ec5136a1749f09
Dare  Badejo <darebadejo@Mac.lan>
2025-04-27 22:02:11 -0400

    updated the data_ingestion.py

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,60 @@
+# here we read datasets from the different data sources and build a data pipeline 
+# here we read the datasets 
+
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str = os.path.join('artifacts', 'train.csv')
+    test_data_path: str = os.path.join('artifacts', 'test.csv')
+    raw_data_path: str = os.path.join('artifacts', 'data.csv')
+    split_ratio: float = 0.2
+    data_source: str = "csv"
+
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+        self.data_source = self.ingestion_config.data_source
+
+    def initiate_data_ingestion(self):
+        logging.info("Data Ingestion method starts")
+        try:
+            df = pd.read_csv('notebook/data/stud.csv')
+            logging.info('read the dataset as dataframe')
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+            logging.info('raw data is saved')
+            logging.info('train test split initiated')
+
+            # Splitting the data into train and test sets
+            # Here we are using the train_test_split function from sklearn to split the data
+            train_set, test_set = train_test_split(df, test_size=self.ingestion_config.split_ratio, random_state=42)
+            logging.info('train test split completed')
+            # Saving the train and test sets to csv files
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            logging.info('train data is saved')
+
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+            logging.info('test data is saved')
+
+            logging.info("Data Ingestion method completed successfully")
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path,
+                self.ingestion_config.raw_data_path
+            )
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+if __name__ == "__main__":
+    data_ingestion = DataIngestion()
+    data_ingestion.initiate_data_ingestion()
